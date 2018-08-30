6eef00e1d7f17db9c848de3be5e5515cc2297c33
o3c9 <at.o3c9@gmail.com>
2018-08-30 17:42:46 +0100

    Add ReadIntent

---
+++ alexa/elements/lambda/custom/index.ts
@@ -0,0 +1,116 @@
+"use strict";
+
+import {
+  ErrorHandler,
+  HandlerInput,
+  RequestHandler,
+  SkillBuilders,
+  RequestInterceptor
+} from "ask-sdk-core";
+
+import { Response, SessionEndedRequest } from "ask-sdk-model";
+
+import {
+  DynamoDbPersistenceAdapter,
+  PartitionKeyGenerators
+} from "ask-sdk-dynamodb-persistence-adapter";
+
+import elements from "../data/elements";
+
+const LaunchRequestHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return handlerInput.requestEnvelope.request.type === "LaunchRequest";
+  },
+  handle(handlerInput: HandlerInput): Response {
+    const speechText = "Alexaで学ぼう、元素記号";
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard("Elements", speechText)
+      .getResponse();
+  }
+};
+
+const HelloElementsIntentHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "HelloWorldIntent"
+    );
+  },
+  handle(handlerInput: HandlerInput): Response {
+    const speechText = "水素・ヘリウム・リチウム・ベリリウム！";
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard("elements", speechText)
+      .getResponse();
+  }
+};
+
+const ReadIntentHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "ReadIntent"
+    );
+  },
+  handle(handlerInput: HandlerInput): Response {
+    const speechText = elements
+      .slice(0, 36)
+      .map(elm => elm.name.ja)
+      .join(", ");
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard("elements", speechText)
+      .getResponse();
+  }
+};
+
+const PersistentAttributesRequestInterceptor: RequestInterceptor = {
+  async process(handlerInput: HandlerInput): Promise<void> {
+    if (handlerInput.requestEnvelope.session.new == true) {
+      const attr = await handlerInput.attributesManager.getPersistentAttributes();
+      const callNum = attr.callNum ? attr.callNum : 0;
+      attr.callNum = callNum + 1;
+      handlerInput.attributesManager.setPersistentAttributes(attr);
+    }
+  }
+};
+
+const PersistentAttributesResponseInterceptor: RequestInterceptor = {
+  async process(handlerInput: HandlerInput): Promise<void> {
+    await handlerInput.attributesManager.savePersistentAttributes();
+  }
+};
+
+const ErrorHandler: ErrorHandler = {
+  canHandle(handlerInput: HandlerInput, error: Error): boolean {
+    return true;
+  },
+  handle(handlerInput: HandlerInput, error: Error): Response {
+    console.log(`エラー: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak("ごめんなさい、もう一度言い直してください")
+      .reprompt("ごめんなさい、もう一度言い直してください")
+      .getResponse();
+  }
+};
+
+const persistenceAdapter = new DynamoDbPersistenceAdapter({
+  tableName: process.env.DYNAMODB_TABLE_NAME,
+  partitionKeyName: process.env.PARTITION_KEY_NAME,
+  partitionKeyGenerator: PartitionKeyGenerators.userId
+});
+
+exports.handler = SkillBuilders.custom()
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    HelloElementsIntentHandler,
+    ReadIntentHandler
+  )
+  .addRequestInterceptors(PersistentAttributesRequestInterceptor)
+  .addResponseInterceptors(PersistentAttributesResponseInterceptor)
+  .addErrorHandlers(ErrorHandler)
+  .withPersistenceAdapter(persistenceAdapter)
+  .lambda();
