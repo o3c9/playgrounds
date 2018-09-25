23fe875901ba5fa2f9b2ebc1878679dc4d0d1e0f
o3c9 <at.o3c9@gmail.com>
2018-09-25 12:43:16 +0100

    Add reference to methods in go

--- golang/introduction/method.go
+++ golang/introduction/method.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+/**
+ * https://skatsuta.github.io/2015/12/29/value-receiver-pointer-receiver/
+**/
+
 // Calc is a number struct
 type Calc struct{ lhs, rhs int }
 
@@ -17,10 +21,20 @@ func (c Calc) Add() int {
 	return c.lhs + c.rhs
 }
 
+// Sub is a receiver method to add members of Calc with a pointer
+func (c *Calc) Sub() int {
+	return c.lhs - c.rhs
+}
+
 func main() {
 	x := Calc{1, 3}
 	fmt.Println(Add(x))
 
 	y := Calc{3, 5}
 	fmt.Println(y.Add())
+	fmt.Println((&y).Add()) // implicit type conversion from *Calc to Calc
+
+	z := Calc{100, 1}
+	fmt.Println(z.Sub()) // implicit type conversion from Calc to *Calc
+	fmt.Println((&z).Sub())
 }
