728d44d91ee89d568256f2cbafad1cf67ae97aab
o3c9 <at.o3c9@gmail.com>
2019-10-11 19:15:40 +0900

    Try Kotlin

---
+++ kotlin/headfirst/2_PhraseOMatic/src/PhraseOMatic.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val wa1 = arrayOf("24/7", "multi-tier", "B-to-B", "dynamic", "pervasive")
+    val wa2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
+    val wa3 = arrayOf("process", "paradigm")
+}
