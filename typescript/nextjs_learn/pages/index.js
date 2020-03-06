6fe133cb65ceff366c881b5b5cad702aa8f1db60
o3c9 <at.o3c9@gmail.com>
2020-03-06 11:38:46 +0900

    Lesson 4 -6 getInitialProps

--- typescript/nextjs_learn/pages/index.js
+++ typescript/nextjs_learn/pages/index.js
@@ -1,9 +1,27 @@
 import Layout from "../components/PageLayout";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-export default function Index() {
-    return (
-        <Layout>
-            <p>Hello Next.js</p>
-        </Layout>
-    );
-}
+const Index = props => (
+    <Layout>
+        <h1>Batman TV Shows</h1>
+        <ul>
+            {(props.shows || []).map(show => (
+                <li key={show.id}>
+                    <Link href="/p/[id]" as={`/p/${show.id}`}>
+                        <a>{show.name}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </Layout>
+);
+
+Index.getInitialProps = async function() {
+    const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+    const data = await res.json();
+    console.log("tvmaze: ", data.length);
+    return { shows: data.map(e => e.show) };
+};
+
+export default Index;
